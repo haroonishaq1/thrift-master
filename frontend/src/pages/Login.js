4d0151af4937ba1b0881849f6c281fd0fd2643f4
Haroon Ishaq <haroonishaq1@gmail.com>
2025-07-02 11:50:53 +0500

    feat: Remove back arrow from Login page and update ProductCard logo styling

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -118,11 +118,6 @@ function Login({ updateAuthStatus }) {
   return (
     <div className="login-container">
       <div className="login-form-wrapper">
-        <div className="back-arrow" onClick={() => navigate('/')}>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M15 18L9 12L15 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-          </svg>
-        </div>
         <h1>THRIFT</h1>
         <h2>Log in to Thrift with me</h2>
         
